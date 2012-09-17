0b04bedccd84be70372b4a8a41f10477e3adddd1
Olivier Hory <olivier.hory@gmail.com>
2012-09-17 14:42:24 +0200

    Update conf of jasmine test suite in nodejs

--- test/testsuite.js
+++ test/testsuite.js
@@ -2,17 +2,20 @@ require('./lib/deps/jquery/dist/jquery.min.js');
 require('./lib/deps/json2.js');
 require('./lib/deps/require.js');
 
-fs = require('fs');
-var base = require('./lib/tetra.js');
-module.exports = tns = base.tns;
-module.exports = tetra = base.tetra;
-require('./lib/mod/require/client.js');
-require('./lib/mod/tmpl/client-micro-tmpl.js');
-require('./lib/tetra-view.js');
-require('./lib/tetra-controller.js');
-require('./lib/tetra-model.js');
+module.exports = function() {
+	fs = require('fs');
+	var base = require('./lib/tetra.js');
+	module.exports = tns = base.tns;
+	module.exports = tetra = base.tetra;
+	require('./lib/mod/require/client.js');
+	require('./lib/mod/tmpl/client-micro-tmpl.js');
+	require('./lib/tetra-view.js');
+	require('./lib/tetra-controller.js');
+	require('./lib/tetra-model.js');
+	require('./spec/javascripts/specs/core/conf/viadeo-webapp.js');
+	return tetra;
+};
 
-require('./spec/javascripts/specs/core/conf/viadeo-webapp.js');
 require('./spec/javascripts/lib/Sinon.JS/sinon-1.4.2.js');
 require('./spec/javascripts/lib/Sinon.JS/sinon-ie-1.4.2.js');
 require('./spec/javascripts/lib/jasmine/lib/jasmine-jquery.js');
