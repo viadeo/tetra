6f96f74706925d82a117f8626e47cb9142947943
Cormac Flynn <cormac.flynn@viadeoteam.com>
2013-04-04 17:26:40 +0200

    Starting implementation of pubsub module

---
+++ src-experiment/src/tetra/_base/topic.js
@@ -0,0 +1,18 @@
+define([], function() {
+
+	// Pub/Sub functionality, based on amplify.js and Dojo
+
+	var subscriptions =  {};
+
+	return {
+		publish: function(topic){
+
+		},
+		subscribe: function(topic, callback) {
+
+		},
+		unsubscribe: function(topic, callback) {
+
+		}
+	}
+});
